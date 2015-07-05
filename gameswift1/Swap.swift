3f713603c181e0e50a1fb9a7fce09f390d09b6b7
phamvanhung <phamvanhung2e123@gmail.com>
2015-07-05 13:01:59 +0900

    check swaps are allowed

--- gameswift1/Swap.swift
+++ gameswift1/Swap.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015年 phamvanhung. All rights reserved.
 //
 
-struct Swap: Printable {
+struct Swap: Printable,Hashable {
     let cookieA: Cookie
     let cookieB: Cookie
     
@@ -14,8 +14,15 @@ struct Swap: Printable {
         self.cookieA = cookieA
         self.cookieB = cookieB
     }
-    
+    var hashValue: Int {
+        return cookieA.hashValue ^ cookieB.hashValue
+    }
+   
     var description: String {
         return "swap \(cookieA) with \(cookieB)"
     }
+}
+func ==(lhs: Swap, rhs: Swap) -> Bool {
+    return (lhs.cookieA == rhs.cookieA && lhs.cookieB == rhs.cookieB) ||
+        (lhs.cookieB == rhs.cookieA && lhs.cookieA == rhs.cookieB)
 }
